163d3f311f78ddea986716e7dd8171aad3f470e5
Keith Chester <kchester@gmail.com>
2018-08-19 13:25:12 -0500

    test: Tests written for queryForGroups route

--- src/tests/Groups.routes.test.ts
+++ src/tests/Groups.routes.test.ts
@@ -76,3 +76,95 @@ describe("/groups - get all groups", ()=>{
         resetUtilities();
     });
 });
+
+describe("/groups/query - Query for groups", ()=>{
+
+    it("should get expected groups based on queries being passed in", async ()=>{
+        let request = createRequest({
+            query: {
+                name: 'sambashare'
+            }
+        });
+        let response = createResponse();
+
+        GroupRoutes.queryForGroups(request, response);
+
+        //Wait for the call to be done
+        await finish(response);
+ 
+        expect(response.statusCode).to.be.equal(200);
+
+        let responseData = response._getData() as GroupItem[];
+        
+        expect(Array.isArray(responseData)).to.be.true;
+        expect(responseData.length).to.be.equal(1);
+        expect(responseData[0].name).to.be.equal("sambashare");
+        expect(responseData[0].members.length).to.be.equal(2);
+    });
+
+    it("should return an empty array if no such groups match", async ()=>{
+        let request = createRequest({
+            query: {
+                name: 'doesntexist'
+            }
+        });
+        let response = createResponse();
+
+        GroupRoutes.queryForGroups(request, response);
+
+        //Wait for the call to be done
+        await finish(response);
+ 
+        expect(response.statusCode).to.be.equal(200);
+
+        let responseData = response._getData() as GroupItem[];
+        
+        expect(Array.isArray(responseData)).to.be.true;
+        expect(responseData.length).to.be.equal(0);
+    });
+
+    it("should return an appropriate status code and error message if the passwd file path is wrong", async ()=>{
+        let newPath = "./doesnt/exist";
+        Group.setPath(newPath);
+
+        let request = createRequest();
+        let response = createResponse();
+
+        GroupRoutes.queryForGroups(request, response);
+
+        //Wait for the call to be done
+        await finish(response);
+
+        expect(response.statusCode).to.be.equal(500);
+        
+        let responseData = response._getData() as ErrorResponse;
+
+        expect(responseData.code).to.be.ok;
+        expect(responseData.code).to.be.equal("GROUP_FILE_LOCATION_ERROR");
+        expect(responseData.message).to.be.equal("Something went wrong reading the group file");
+    });
+
+    it("should return an appropriate status code and error message if the group file can not be parsed", async ()=>{
+        Group.setLineDelimiter("\t");
+
+        let request = createRequest();
+        let response = createResponse();
+
+        GroupRoutes.queryForGroups(request, response);
+
+        //Wait for the call to be done
+        await finish(response);
+
+        expect(response.statusCode).to.be.equal(500);
+
+        let responseData = response._getData() as ErrorResponse;
+
+        expect(responseData.code).to.be.ok;
+        expect(responseData.code).to.be.equal("GROUP_PARSE_ERROR");
+        expect(responseData.message).to.be.equal('There was an issue parsing the group file');
+    });
+
+    beforeEach(()=>{
+        resetUtilities();
+    });
+});
