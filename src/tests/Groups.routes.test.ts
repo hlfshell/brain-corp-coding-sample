8c8f938bcdc9411521db48092bd3d9bc7c4df408
Keith Chester <kchester@gmail.com>
2018-08-19 12:58:55 -0500

    test: Initial tests written for /groups route

--- src/tests/Groups.routes.test.ts
+++ src/tests/Groups.routes.test.ts
@@ -10,3 +10,70 @@ import PasswdUser from '../interfaces/PasswdUser';
 import { finish } from "./finish";
 import ErrorResponse from '../interfaces/ErrorResponse';
 import GroupItem from '../interfaces/GroupItem';
+
+describe("/groups - get all groups", ()=>{
+
+    it("should return a list of all groups if all goes well", async ()=>{
+        let request = createRequest();
+        let response = createResponse();
+
+        GroupRoutes.getAllGroups(request, response);
+
+        //Wait for the call to be done
+        await finish(response);
+ 
+        expect(response.statusCode).to.be.equal(200);
+
+        let responseData = response._getData() as GroupItem[];
+        
+        expect(Array.isArray(responseData)).to.be.true;
+        expect(responseData.length).to.be.equal(23);
+    });
+
+    it("should return an appropriate status code and error message if the passwd file path is wrong", async ()=>{
+        let newPath = "./doesnt/exist";
+        Passwd.setPath(newPath);
+
+        let request = createRequest();
+        let response = createResponse();
+
+        GroupRoutes.getAllGroups(request, response);
+
+        //Wait for the call to be done
+        await finish(response);
+
+        expect(response.statusCode).to.be.equal(500);
+        
+        let responseData = response._getData() as ErrorResponse;
+
+        expect(responseData.code).to.be.ok;
+        expect(responseData.code).to.be.equal("GROUP_FILE_LOCATION_ERROR");
+        expect(responseData.message).to.be.equal("Something went wrong reading the group file");
+    });
+
+    it("should return an appropriate status code and error message if the group file can not be parsed", async ()=>{
+        Passwd.setLineDelimiter("\t");
+
+        let request = createRequest();
+        let response = createResponse();
+
+        GroupRoutes.getAllGroups(request, response);
+
+        //Wait for the call to be done
+        await finish(response);
+
+        expect(response.statusCode).to.be.equal(500);
+
+        let responseData = response._getData() as ErrorResponse;
+
+        expect(responseData.code).to.be.ok;
+        expect(responseData.code).to.be.equal("GROUP_PARSE_ERROR");
+        expect(responseData.message).to.be.equal('There was an issue parsing the group file');
+    });
+
+    beforeEach(()=>{
+        Group.setPath("./src/tests/fake.group");
+        Group.setLineDelimiter("\n");
+        Group.setColumnDelimiter(":");
+    });
+});
