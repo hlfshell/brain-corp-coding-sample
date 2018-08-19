df7313e7b2da7ca59a0cc7d855a54260cf945226
Keith Chester <kchester@gmail.com>
2018-08-18 19:23:57 -0500

    test: Finished tests for query function for passwd

--- src/tests/Passwd.test.ts
+++ src/tests/Passwd.test.ts
@@ -84,6 +84,14 @@ describe("Passwd - getAllUsers", ()=>{
         expect(Array.isArray(users)).to.be.true;
     });
 
+    it("should return the correct number of members", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let users = await passwd.getAllUsers();
+
+        expect(users).to.be.ok;
+        expect(users.length).to.be.equal(25);
+    });
+
     it("each object should return a username, uid, gid, comment, home directory, and shell", async ()=>{
         let users = await passwd.getAllUsers();
 
@@ -111,6 +119,21 @@ describe("Passwd - getAllUsers", ()=>{
 
         expect(user.shell).to.not.be.null;
         expect(typeof user.shell).to.be.equal("string");
+    });
+
+    it("should return a member with information properly set", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let users = await passwd.getAllUsers();
+
+        let keith = users[24];
+
+        expect(keith).to.be.ok;
+        expect(keith.name).to.be.equal("keith");
+        expect(keith.uid).to.be.equal(1000);
+        expect(keith.gid).to.be.equal(1000);
+        expect(keith.comment).to.be.equal("Keith Chester,,,");
+        expect(keith.home).to.be.equal("/home/keith");
+        expect(keith.shell).to.be.equal("/bin/bash");
     });
 
     it("should fail if the passwd file does not exist", async ()=>{
@@ -145,6 +168,150 @@ describe("Passwd - getAllUsers", ()=>{
         
         //We should never reach here due to the catch above
         expect.fail();
+    });
+
+    afterEach(()=>{
+        Passwd.setPath("/etc/passwd");
+        Passwd.setLineDelimiter("\n");
+        Passwd.setColumnDelimiter(":");
+    });
+
+});
+
+describe("Passwd - getUsersByQuery", ()=>{
+
+    let Passwd = require('../classes/Passwd').Passwd;
+    let passwd = Passwd.getInstance();
+
+    it("should return an array with the correct members from the query", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let queriedUsers = await passwd.getUsersByQuery({ name: 'keith' });
+
+        expect(queriedUsers).to.be.ok;
+        expect(Array.isArray(queriedUsers)).to.be.true;
+        expect(queriedUsers.length).to.be.equal(1);
+
+        let keith = queriedUsers[0];
+
+        expect(keith).to.be.ok;
+        expect(keith.name).to.be.equal("keith");
+        expect(keith.uid).to.be.equal(1000);
+        expect(keith.gid).to.be.equal(1000);
+        expect(keith.comment).to.be.equal("Keith Chester,,,");
+        expect(keith.home).to.be.equal("/home/keith");
+        expect(keith.shell).to.be.equal("/bin/bash");
+    });
+
+    it("should return an emtpy array if no users match the query", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let queriedUsers = await passwd.getUsersByQuery({ name: "doesn't_exist" });
+
+        expect(queriedUsers).to.be.ok;
+        expect(Array.isArray(queriedUsers)).to.be.true;
+        expect(queriedUsers.length).to.be.equal(0);
+    });
+
+    it("should allow querying by name", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let queriedUsers = await passwd.getUsersByQuery({ name: "daemon" });
+
+        expect(queriedUsers).to.be.ok;
+        expect(Array.isArray(queriedUsers)).to.be.true;
+        expect(queriedUsers.length).to.be.equal(1);
+    });
+
+    it("should allow querying by uid", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let queriedUsers = await passwd.getUsersByQuery({  uid: 39 });
+
+        expect(queriedUsers).to.be.ok;
+        expect(Array.isArray(queriedUsers)).to.be.true;
+        expect(queriedUsers.length).to.be.equal(1);
+    });
+
+    it("should allow querying by gid", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let queriedUsers = await passwd.getUsersByQuery({ gid: 34 });
+
+        expect(queriedUsers).to.be.ok;
+        expect(Array.isArray(queriedUsers)).to.be.true;
+        expect(queriedUsers.length).to.be.equal(1);
+    });
+
+    it("should allow querying by comment", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let queriedUsers = await passwd.getUsersByQuery({ comment: "systemd Time Synchronization,,," });
+
+        expect(queriedUsers).to.be.ok;
+        expect(Array.isArray(queriedUsers)).to.be.true;
+        expect(queriedUsers.length).to.be.equal(1);
+    });
+
+    it("should allow querying by home", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let queriedUsers = await passwd.getUsersByQuery({ home: "/home/keith" });
+
+        expect(queriedUsers).to.be.ok;
+        expect(Array.isArray(queriedUsers)).to.be.true;
+        expect(queriedUsers.length).to.be.equal(1);
+    });
+
+    it("should allow querying by shell", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let queriedUsers = await passwd.getUsersByQuery({ shell: "/bin/bash" });
+
+        expect(queriedUsers).to.be.ok;
+        expect(Array.isArray(queriedUsers)).to.be.true;
+        expect(queriedUsers.length).to.be.equal(2);
+    });
+
+    it("should allow querying by mixed attributes", async ()=>{
+        Passwd.setPath("./src/tests/fake.passwd");
+        let queriedUsers = await passwd.getUsersByQuery({ name: 'keith', uid: 1000, gid: 1000, comment: "Keith Chester,,," });
+
+        expect(queriedUsers).to.be.ok;
+        expect(Array.isArray(queriedUsers)).to.be.true;
+        expect(queriedUsers.length).to.be.equal(1);
+    });
+
+    it("should fail if the passwd file does not exist", async ()=>{
+        Passwd.setPath("/does/not/exist");
+
+        // Note - normally you'd do a expect(fnc).to.throw() - can't do
+        // that with async it seems?
+
+        try{
+            let users = await passwd.getAllUsers();
+        } catch(err){
+            expect(err instanceof Error).to.be.true;
+            expect(err.message).to.be.equal(`Something went wrong reading the passwd file`);
+            return; // We want to stop here
+        }
+
+        //We should never reach here due to the catch above
+        expect.fail();
+    });
+
+    it("should fail if there is a problem parsing the passwd file", async ()=>{
+        Passwd.setLineDelimiter("\t");
+        Passwd.setColumnDelimiter("|");
+        let users;
+        try{
+            users = await passwd.getAllUsers();
+        } catch(err){
+            expect(err instanceof Error).to.be.true;
+            expect(err.message).to.be.equal("There was an issue parsing the passwd file");
+            return
+        }
+        
+        //We should never reach here due to the catch above
+        expect.fail();
+    });
+
+    afterEach(()=>{
+        Passwd.setPath("/etc/passwd");
+        Passwd.setLineDelimiter("\n");
+        Passwd.setColumnDelimiter(":");
     });
 
     afterEach(()=>{
