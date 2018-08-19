c8fb3e69a9f689d5a726c80546521cb699717d7b
Keith Chester <kchester@gmail.com>
2018-08-19 11:54:59 -0500

    test: Finished user routes get all users tests

---
+++ src/interfaces/ErrorResponse.ts
@@ -0,0 +1,4 @@
+export default interface ErrorResponse {
+    code: string, //short machine readable error code
+    message: string, //human readbale explanation
+}
--- src/tests/User.routes.test.ts
+++ src/tests/User.routes.test.ts
@@ -6,7 +6,8 @@ import { createRequest, createResponse } from "node-mocks-http";
 
 import UserRoutes from "../routes/users";
 import PasswdUser from '../interfaces/PasswdUser';
-import { awaitFinish } from "./awaitFinish";
+import { finish } from "./finish";
+import ErrorResponse from '../interfaces/ErrorResponse';
 
 describe("/users - Get all users", ()=>{
 
@@ -20,17 +21,54 @@ describe("/users - Get all users", ()=>{
         UserRoutes.getAllUsers(request, response);
 
         //Wait for the call to be done
-        await awaitFinish(response);
+        await finish(response);
  
+        expect(response.statusCode).to.be.equal(200);
+
         let responseData = JSON.parse(response._getData()) as PasswdUser[];
         
         expect(Array.isArray(responseData)).to.be.true;
         expect(responseData.length).to.be.equal(25);
     });
 
-    it("should return an appropriate status code and error message if the passwd file path is wrong");
+    it("should return an appropriate status code and error message if the passwd file path is wrong", async ()=>{
+        let passwd = Passwd.getInstance();
+        let newPath = "./src/tests/fake.passwd";
+        Passwd.setPath(newPath);
 
-    it("should return an appropriate status code and error message if the passwd file can not be parsed");
+        let request = createRequest();
+        let response = createResponse();
+
+        //Wait for the call to be done
+        await finish(response);
+
+        expect(response.statusCode).to.be.equal(500);
+
+        let responseData = JSON.parse(response._getData()) as ErrorResponse;
+
+        expect(responseData.code).to.be.ok;
+        expect(responseData.code).to.be.equal("PASSWD_FILE_LOCATION_ERROR");
+        expect(responseData.message).to.be.equal(`The passwd file could not be reached at ${newPath}`);
+    });
+
+    it("should return an appropriate status code and error message if the passwd file can not be parsed", async ()=>{
+        let passwd = Passwd.getInstance();
+        Passwd.setPath("./src/tests/fake.passwd");
+
+        let request = createRequest();
+        let response = createResponse();
+
+        //Wait for the call to be done
+        await finish(response);
+
+        expect(response.statusCode).to.be.equal(500);
+
+        let responseData = JSON.parse(response._getData()) as ErrorResponse;
+
+        expect(responseData.code).to.be.ok;
+        expect(responseData.code).to.be.equal("PASSWD_PARSE_ERROR");
+        expect(responseData.message).to.be.equal('There was an issue parsing the passwd file');
+    });
 
 
     afterEach(()=>{
