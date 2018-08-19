241c2b98782da239e1e6be98b9e649eb9648fd1a
Keith Chester <kchester@gmail.com>
2018-08-19 12:32:43 -0500

    feature: Wrapped repeating catch code to its own function

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,34 +6,38 @@ import ErrorResponse from "../interfaces/ErrorResponse";
 
 let passwd = Passwd.getInstance();
 
+function handleErrors(err : Error, res : Response){
+    let error : ErrorResponse = {
+        code: "",
+        message: err.message
+    }
+
+    switch(err.message){
+        case "Something went wrong reading the passwd file": {
+            error.code = "PASSWD_FILE_LOCATION_ERROR";
+            break;
+        }
+        case "There was an issue parsing the passwd file": {
+            error.code = "PASSWD_PARSE_ERROR";
+            break;
+        }
+        default: {
+            error.code = "UNKNOWN_ERROR";
+            break;
+        }
+    }
+
+    res.status(500).send(error);
+};
+
 let UserRoutes : Route = {
-    
+
     getAllUsers: async function(req : Request, res : Response){
         try{
             let allUsers = await passwd.getAllUsers();
             res.send(allUsers);
         } catch(err){
-            let error : ErrorResponse = {
-                code: "",
-                message: err.message
-            }
-
-            switch(err.message){
-                case "Something went wrong reading the passwd file": {
-                    error.code = "PASSWD_FILE_LOCATION_ERROR";
-                    break;
-                }
-                case "There was an issue parsing the passwd file": {
-                    error.code = "PASSWD_PARSE_ERROR";
-                    break;
-                }
-                default: {
-                    error.code = "UNKNOWN_ERROR";
-                    break;
-                }
-            }
-
-            res.status(500).send(error);
+            handleErrors(err, res);
         }
     },
 
@@ -42,27 +46,7 @@ let UserRoutes : Route = {
             let users = await passwd.getUsersByQuery(req.query);
             res.send(users);
         } catch(err){
-            let error : ErrorResponse = {
-                code: "",
-                message: err.message
-            }
-
-            switch(err.message){
-                case "Something went wrong reading the passwd file": {
-                    error.code = "PASSWD_FILE_LOCATION_ERROR";
-                    break;
-                }
-                case "There was an issue parsing the passwd file": {
-                    error.code = "PASSWD_PARSE_ERROR";
-                    break;
-                }
-                default: {
-                    error.code = "UNKNOWN_ERROR";
-                    break;
-                }
-            }
-
-            res.status(500).send(error);
+            handleErrors(err, res);
         }
     },
 
