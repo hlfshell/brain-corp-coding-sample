88c814d0288ef7f1a63b4ee3e685a325171d83e9
Keith Chester <kchester@gmail.com>
2018-08-19 12:08:17 -0500

    feature: getAllUsers route finished as per tests

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,40 @@
 import { Request, Response } from "express";
 
 import { Route } from "../interfaces/Route";
+import { Passwd } from "../classes/Passwd";
+import ErrorResponse from "../interfaces/ErrorResponse";
+
+let passwd = Passwd.getInstance();
 
 let UserRoutes : Route = {
     
-    getAllUsers: function(req : Request, res : Response){
-        
+    getAllUsers: async function(req : Request, res : Response){
+        try{
+            let allUsers = await passwd.getAllUsers();
+            res.send(allUsers);
+        } catch(err){
+            let error : ErrorResponse = {
+                code: "",
+                message: err.message
+            }
+
+            switch(err.message){
+                case "Something went wrong reading the passwd file": {
+                    error.code = "PASSWD_FILE_LOCATION_ERROR";
+                    break;
+                }
+                case "There was an issue parsing the passwd file": {
+                    error.code = "PASSWD_PARSE_ERROR";
+                    break;
+                }
+                default: {
+                    error.code = "UNKNOWN_ERROR";
+                    break;
+                }
+            }
+            
+            res.status(500).send(error);
+        }
     },
 
     queryForUsers: function(req : Request, res : Response){
