71a10d82cbc27939a9e173448511b08110120117
Keith Chester <kchester@gmail.com>
2018-08-19 14:26:47 -0500

    feature: Changes to server to auto select port if not used, routes defined

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,37 @@
 import express from "express";
 import { Express } from "express";
 
+import GroupRoutes from "./routes/groups";
+import UserRoutes from "./routes/users";
+
+import getPort = require('get-port');
+
 export default class Server {
 
     port : number;
     app : Express;
 
-    constructor(port : number){
+    constructor(port : number = -1){
         this.port = port;
         this.app = express();
         this.loadRoutes();
     }
 
     private loadRoutes() : void {
-        
+        // /users
+        this.app.get("/users", UserRoutes.getAllUsers);
+        this.app.get("/users/query", UserRoutes.queryForUsers);
+        this.app.get("/users/:uid");
+        this.app.get("/users/:uid/groups");
+
+        // /groups
+        this.app.get("/groups", GroupRoutes.getAllGroups);
+        this.app.get("/groups/query", GroupRoutes.queryForGroups);
+        this.app.get("/groups/:gid", GroupRoutes.getSpecificGroup);
     }
 
-    public listen() : void {
+    public async listen() : Promise<void> {
+        if(this.port == -1) this.port = await getPort();
         this.app.listen(this.port, ()=> console.log(`Server is now listening on ${this.port}!`));
     }
 
