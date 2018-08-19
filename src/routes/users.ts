444577b8bf90898e893e23e95e82944ec2b43fed
Keith Chester <kchester@gmail.com>
2018-08-19 12:26:31 -0500

    feature: getUsersByQuery route written as per tests

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -32,13 +32,38 @@ let UserRoutes : Route = {
                     break;
                 }
             }
-            
+
             res.status(500).send(error);
         }
     },
 
-    queryForUsers: function(req : Request, res : Response){
+    queryForUsers: async function(req : Request, res : Response){
+        try {
+            let users = await passwd.getUsersByQuery(req.params);
+            res.send(users);
+        } catch(err){
+            let error : ErrorResponse = {
+                code: "",
+                message: err.message
+            }
 
+            switch(err.message){
+                case "Something went wrong reading the passwd file": {
+                    error.code = "PASSWD_FILE_LOCATION_ERROR";
+                    break;
+                }
+                case "There was an issue parsing the passwd file": {
+                    error.code = "PASSWD_PARSE_ERROR";
+                    break;
+                }
+                default: {
+                    error.code = "UNKNOWN_ERROR";
+                    break;
+                }
+            }
+
+            res.status(500).send(error);
+        }
     },
 
     getSpecificUser: function(req : Request, res : Response){
